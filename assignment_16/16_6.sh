a45339a3d496c1bcb126af93340102e4cec41b23
C1-80781 <144808226+C1-80781@users.noreply.github.com>
2023-11-03 23:32:43 +0530

    Create 16_6.sh

---
+++ assignment_16/16_6.sh
@@ -0,0 +1,20 @@
+
+echo "Enter a year:"
+read year
+
+if (( year % 4 == 0 ))
+then
+   if (( year % 100 == 0 ))
+   then
+       if (( year % 400 == 0 ))
+       then
+           echo "$year is a leap year."
+       else
+           echo "$year is not a leap year."
+       fi
+   else
+       echo "$year is a leap year."
+   fi
+else
+   echo "$year is not a leap year."
+fi
